2d5ad6f47191074896b22bf26a6c043b93005bfe
anofibonacci <fibonacci.php@gmail.com>
2023-08-24 19:04:03 -0700

    cleaned up photo-only posts, including multiples; added more photos;

--- src/lib/components/PhotoOnlyFrame.svelte
+++ src/lib/components/PhotoOnlyFrame.svelte
@@ -1,25 +1,15 @@
 <script lang="ts">
-	import ndk from '$lib/stores/ndk';
-	import type { NDKEvent, NDKUser } from '@nostr-dev-kit/ndk';
+	import type { NDKEvent } from '@nostr-dev-kit/ndk';
 	import dayjs from 'dayjs';
 
 	export let post: NDKEvent;
 	//console.log("photo only post: ", post);
 
-	async function getAuthorProfile() {
-		let author: NDKUser = $ndk.getUser({ hexpubkey: post.pubkey });
-		author.fetchProfile();
-		const authorName = (await author.profile?.displayName)
-			? author.profile.displayName
-			: author.profile?.name
-			? author.profile.name
-			: 'anonymous';
-		console.log('author: ', authorName);
-		return authorName;
-	}
-
 	let content: string = post.content;
-	let rTag: number = 0;
+	let source: string = '';
+
+	let photos: string[] = [];
+	//console.log('photos: ', photos);
 
 	// grabbing images that are in tags (type r)
 	function isImageLink(text: string) {
@@ -33,43 +23,52 @@
 		//var pseudoUrlPattern = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
 		var matchedUrls = text.match(urlPattern);
 		if (matchedUrls) {
-			console.log('matchedUrls: ', matchedUrls);
+			//console.log('matchedUrls: ', matchedUrls);
 			return matchedUrls;
 		} else {
-			console.log('no matched Urls');
+			//console.log('no matched Urls');
 			return false;
+		}
+	}
+
+	async function checkForPhotos() {
+		if (post.tags.length > 0) {
+			for (let tag of post.tags) {
+				if (tag[0] == 'r' && isImageLink(tag[1])) {
+					photos.push(tag[1]);
+					source = 'tag';
+				}
+			}
+		} else if (grabImagesFromPost(content)) {
+			for (let imageUrl of grabImagesFromPost(content)) {
+				photos.push(imageUrl);
+				source = 'embed';
+			}
+		} else {
+			console.log('no photos');
 		}
 	}
 </script>
 
-{#if post.tags.length > 0}
-	<div class="photoOnlyBlock">
-		{#each Array.from(post.tags) as tag}
-			{#if tag[0] == 'r' && isImageLink(tag[1])}
-				{#if rTag == 0}<!-- want to move <div> to here but won't compile -->{/if}
-				<img src={tag[1]} alt="tagged from user: {post.pubkey.substring(0, 9)}" />
-			{/if}
-		{/each}
-		<span>
-			{#await getAuthorProfile()}
-				<p>loading...</p>
-			{:then authorName}<p>{authorName} {post.pubkey}</p>
-			{/await}
-		</span>
-	</div>
-{:else if grabImagesFromPost(content)}
-	<div class="photoOnlyBlock">
-		{#each Array.from(grabImagesFromPost(content)) as imageUrl}
-			<img src={imageUrl} alt="embedded from user: {post.pubkey.substring(0, 9)}" />
-		{/each}
-		<span>
-			{#await getAuthorProfile()}
-				<p>loading...</p>
-			{:then authorName}<p>{authorName} {post.pubkey}</p>
-			{/await}
-		</span>
-	</div>
-{/if}
+{#await checkForPhotos()}
+	<p>loading...</p>
+{:then}
+	{#if photos.length > 0}
+		<div class="photoOnlyBlock">
+			<h6>
+				{dayjs.unix(post.created_at ?? 0).format('MMM D, YYYY h:mm a')}<br />&nbsp;
+			</h6>
+			{#each photos as photo}
+				<a href={photo} target="_blank"
+					><img src={photo} alt="{source} from user: {post.pubkey.substring(0, 9)}" /></a
+				>
+			{/each}
+			<p>posted ({source}) by: {post.pubkey}</p>
+		</div>
+	{/if}
+{:catch error}
+	<p>error: {error.message}</p>
+{/await}
 
 <style>
 	h6 {
@@ -82,7 +81,7 @@
 		justify-content: center;
 		width: 100%;
 		padding: 1rem;
-		background-color: var(--surface-4);
+		background-color: var(--surface-5);
 		border: 1px solid var(--color-gray-2);
 		border-radius: var(--size-3);
 		box-shadow: 0 0 0.5rem var(--color-shadow);
